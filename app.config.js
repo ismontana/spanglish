74a9a6d2bd81cd98914d9f21e686a6a464b4db65
Krlostks <carlospixel13@gmail.com>
2025-06-17 11:38:52 -0600

    apk compilation

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-{
+export default {
   "expo": {
     "name": "Spanglish",
     "slug": "Spanglish",
@@ -17,7 +17,10 @@
         "backgroundColor": "#ffffff"
       },
       "edgeToEdgeEnabled": true,
-      "package": "com.ismontana.Spanglish"
+      "package": "com.ismontana.Spanglish",
+      "permissions": [
+                  "android.permission.RECORD_AUDIO"
+                  ]
     },
     "web": {
       "bundler": "metro",
@@ -34,7 +37,22 @@
           "resizeMode": "contain",
           "backgroundColor": "#ffffff"
         }
-      ]
+      ],
+      [
+        "expo-build-properties",
+        {
+          "android": {
+            "manifest": {
+              "xmlns:tools": "http://schemas.android.com/tools"
+            },
+            application: {
+              "tools:replace": "android:appComponentFactory",
+            },
+            "useAndroidX": true,
+            "enableJetifier": true
+          },
+        },
+        ],
     ],
     "experiments": {
       "typedRoutes": true
@@ -42,7 +60,7 @@
     "extra": {
       "router": {},
       "eas": {
-        "projectId": "fea6ab4d-df01-4657-8606-87c2fde9a301"
+        "projectId": "473783a4-e8b8-4251-8b06-72ae9ad48c73"
       }
     }
   }
